4c1efd84a35fc58882c7840a80374b97d973f947
KailonFreire <kscarregosa@gmail.com>
2023-05-08 22:22:20 -0300

    Segunda tela adicionada

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-from PyQt5 import uic;
-import sys;
+import sys
+from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from saque import TelaSaque;
-
-notas = [10,20,50,100];
+from saque import TelaSaque
+notas = [10, 20, 50, 100]
 ui_file = "C:/Users/kaiki/Desktop/Coding/projetopython/interface/telainicial.ui"
 Ui_MainWindow, _ = uic.loadUiType(ui_file)
-
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
-  
+        self.tela_saque = TelaSaque(self)
+        self.saqueBtn.clicked.connect(self.abrirTelaSaque)
+        
+    def abrirTelaSaque(self):
+        self.close()
+        self.tela_saque.show()
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    
     main_window = MainWindow()
-    withdraw_window = TelaSaque()
-   
-
     main_window.show()
-
-    main_window.saqueBtn.clicked.connect(TelaSaque.show)
-
     sys.exit(app.exec_())
--- saque.py
+++ saque.py
@@ -1,55 +1,42 @@
-from PyQt5 import uic;
-import sys;
-from PyQt5.QtWidgets import QApplication, QMainWindow;
-
-notas = [10,20,50,100];
+from PyQt5 import uic
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+notas = [10,20,50,100]
 ui_file = "C:/Users/kaiki/Desktop/Coding/projetopython/interface/tela_saque.ui"
 Ui_MainWindow, _ = uic.loadUiType(ui_file)
-
 class TelaSaque(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super(TelaSaque, self).__init__()
+    def __init__(self, parent=None):
+        super(TelaSaque, self).__init__(parent)
         self.setupUi(self)
-
         self.saque20.clicked.connect(lambda: self.set_valor(20))
         self.saque50.clicked.connect(lambda: self.set_valor(50))
         self.saque100.clicked.connect(lambda: self.set_valor(100))
         self.saque150.clicked.connect(lambda: self.set_valor(150))
-         
         self.confirmarBtn.clicked.connect(self.calcularNotas)
-
         self.saldo = 2000
         self.notas = [10, 20, 50, 100]
         self.valor = 0
-
     def setValorInserido(self, value):
         if self.inserirValor.text() is not None:
             self.set_valor(self.inserirValor.text())
-
-
     def set_valor(self, value):
         try:
             self.valor = int(value)
             print(self.valor)
         except ValueError:
-            print("deu probrema");
-            
+            print("deu probrema")
     def calcularNotas(self):
         resto = self.valor
         resultado = []
-
         for nota in self.notas[::-1]:
             quantidade_notas = resto // nota
             resto = resto % nota
-
             if quantidade_notas > 0:
                 resultado.append((quantidade_notas, nota))
-
-        
             print(f"Serão {quantidade_notas} notas de {nota}")
-
+            
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = TelaSaque()
-    window.show()
-    sys.exit(app.exec_())
+ app = QApplication(sys.argv)
+ window = TelaSaque()
+ window.show()
+ sys.exit(app.exec_())
